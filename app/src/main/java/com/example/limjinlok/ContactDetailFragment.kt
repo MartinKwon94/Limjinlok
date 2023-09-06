c67b8c950cbebf0157de21a5dcab499f723cfa2f
Martin Kwon <mindori9097@gmail.com>
2023-09-07 07:24:27 +0900

    [FEAT] : ⚙️ binding과 bundle진행중..

--- app/src/main/java/com/example/limjinlok/ContactDetailFragment.kt
+++ app/src/main/java/com/example/limjinlok/ContactDetailFragment.kt
@@ -1,52 +1,51 @@
 package com.example.limjinlok
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.limjinlok.databinding.FragmentContactDetailBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ContactDetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ContactDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentContactDetailBinding? = null
+    private val binding get() = _binding!!
+
+    //데이터 원본 필요
+//    private val data: 어댑터? by lazy {
+//        arguments?.getParcelable<어댑터>
+//    }
+//    lateinit var receiveData: 어댑터
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contact_detail, container, false)
+        _binding = FragmentContactDetailBinding.inflate(inflater, container, false)
+
+//        arguments?.getInt(image)
+//        binding.Name.text =
+//        binding.Number.text =
+//        binding.Nickname.text =
+//            //어댑터에서 받아오기?
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ContactDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             ContactDetailFragment().apply {
@@ -55,5 +54,11 @@ class ContactDetailFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+
+
+        override fun onDestroyView() {
+            super.onDestroyView()
+            _binding = null
+        }
     }
 }
