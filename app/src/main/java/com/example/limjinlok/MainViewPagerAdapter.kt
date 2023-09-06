9b8a01a420cb7565aa82ede3e0b3b11e4949b0ef
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-06 21:06:12 +0900

    [FEAT] : ⚙️ add tab data

--- app/src/main/java/com/example/limjinlok/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/limjinlok/MainViewPagerAdapter.kt
@@ -7,6 +7,11 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class MainViewPagerAdapter(fragmentManager: FragmentActivity) :
     FragmentStateAdapter(fragmentManager) {
 
+    private val tabList = ArrayList<MainTabsModel>()
+
+    fun addTabList(fragment: Fragment, title: String) {
+        tabList.add(MainTabsModel(fragment, title))
+    }
     override fun getItemCount(): Int = tabList.size
 
     override fun createFragment(position: Int): Fragment {
