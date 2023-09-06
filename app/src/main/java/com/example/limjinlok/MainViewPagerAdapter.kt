b9cd85bf422c3862de04f4f3d21ad3846a5e1147
Jjol0twi <97600168+Jjol0twi@users.noreply.github.com>
2023-09-06 21:05:27 +0900

    [FEAT] : ⚙️ add viewpager apdater with fragmentstatadpater

--- app/src/main/java/com/example/limjinlok/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/limjinlok/MainViewPagerAdapter.kt
@@ -1,4 +1,15 @@
 package com.example.limjinlok
 
-class MainViewPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class MainViewPagerAdapter(fragmentManager: FragmentActivity) :
+    FragmentStateAdapter(fragmentManager) {
+
+    override fun getItemCount(): Int = tabList.size
+
+    override fun createFragment(position: Int): Fragment {
+        return tabList[position].fragment
+    }
 }
